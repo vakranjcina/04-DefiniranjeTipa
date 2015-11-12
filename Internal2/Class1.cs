706bcb7802a03f1a97c08a767577bc1f64399673
vakranjcina <valentina.kranjcina@hotmail.com>
2015-11-12 20:20:19 +0100

    added a class

---
+++ Internal2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vsite.CSharp
+{
+    class Class1
+    {
+        public void NekaMetoda()
+        {
+            JavnaBazna jb = new JavnaBazna();
+            InternaBazna ib = new InternaBazna();
+        }
+    }
+}
